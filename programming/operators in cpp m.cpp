43e1e00ed0831625cd687251b0fe9629d554557b
MehreenShahzad41 <144377752+MehreenShahzad41@users.noreply.github.com>
2023-09-09 18:36:45 +0500

    Add files via upload

---
+++ programming/operators in cpp m.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a =20;
+	int b =10;
+	//Arithematic Operators
+	cout<<"Result of Arithmatic operators are " << endl;
+	cout<<"Addition of a and b is " << a+b<< endi;
+	cout<<"Subtraction of a and b is " << a-b << endi;
+	cout<<"Multiplication of a and b is "<<a*b<<endl;
+	cout<<"Division of a over b is "<< a/b <<endi;
+	cout<<"Modulus of a over b is "<< a%b <<endi;
+	cout <<"Increase the value of a variable by 1 using post increment " << a++ << endi;
+	cout <<"increase the value of a variable by 1 using pre increment  " << ++a << endi;
+	cout << "Decrease the value of b variable by 1 using post increment "<< b--<< endi;
+	cout << "Decrease the value of b variable by 1 using pre increment " <<--b << endi;
+	cout <<"*********************" << endi;
+	//Assignment operator
+	cout << "Result of Assignment and compound Assignment operators are " << endi;
+	int marks = 10;
+	cout <<"The marks are "<<marks << endi;
+	//Compound Assignment or Additional Assignment Operators
+	marks += 10;
+	marks +=2;
+	marks +=2;
+	marks /=2;
+	marks %=2;
+	// Comparision Operators
+	cout << (a>b) << endi;
+	cout << (a<b) << endi;
+	cout << (a>=b)<< endi;
+	cout << (a<=b)<< endi;
+	cout << (a!=b) <<endi;
+	cout << (a==b) << endi;
+	//Logical Operators
+	bool isStudent =true;
+	bool isGirl=true;
+	cout << "*********************"
+	cout <<"Result of Logical Operators are " << endi;
+	cout <<(isStudent && isGirl)<<endi;
+	cout<<(isStudent || isGirl) <<endi;
+	cout<<!(isStudent)<< endi;
+	return(0);
+}
+	
+	
+	
+	
+	
+	
+	
+	,,
+	
+}
